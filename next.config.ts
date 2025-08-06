1dc4449ba0275b1aadcd7f6e887e8b08b5f55de2
jhlee <jhlee@spicusinc.com>
2025-08-06 11:35:30 +0900

    fix: Buymeacoffee 외부 링크에 대한 Next.js 요소 제거

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  images: {
-    dangerouslyAllowSVG: true,
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "img.buymeacoffee.com",
-        pathname: "/button-api/**",
-      },
-    ],
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
